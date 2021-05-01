46934cd0d2f28995acb9872f8952fa97b7f363ab
Hugues Morisset <morisset.hugues@gmail.com>
2021-05-01 18:49:09 +0200

    Beautify code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 use anyhow::{anyhow, Result};
 use clap::{crate_name, crate_version, App as Clapp, Arg};
-use log::info;
+use log::*;
 use std::{env::set_var, env::var};
 
-use dbus::message::MatchRule;
-use dbus::nonblock::stdintf::org_freedesktop_dbus::Properties;
-use dbus::nonblock::Proxy;
-use dbus::Path;
+use dbus::{
+    message::MatchRule,
+    nonblock::{stdintf::org_freedesktop_dbus::Properties, MsgMatch, Proxy},
+    Path,
+};
 use dbus_tokio::connection;
-use futures::channel;
-use tokio::stream::StreamExt;
-
-use dbus::nonblock::MsgMatch;
+use futures::{channel, stream::StreamExt};
 
 use std::collections::HashMap;
-use std::time::Duration;
 use std::process::Command;
+use std::time::Duration;
 
 mod config;
 use config::{Config, ConnectionConfig};
 
 const CONFIG_FILE_ENV: &str = "NM_DBUS_CONNECTION_ACTION_CONFIG";
 // const DBUS_NM_PATH: &str = "/org/freedesktop/NetworkManager";
+const DBUS_NM_ACTIVE_CONNECTION_PATH: &str = "/org/freedesktop/NetworkManager/ActiveConnection/";
 const DBUS_NM_OBJECT_NAME: &str = "org.freedesktop.NetworkManager";
-const DEFAULT_TIMEOUT: u64 = 1000;
+const DBUS_DEFAULT_TIMEOUT: u64 = 1000;
 
 // `nmcli c` to get connections uuid
 
-#[derive(Debug, PartialEq)]
-enum ActiveConnectionAction {
+/// Event on a watched connection
+#[derive(Debug, PartialEq, Clone)]
+enum ConnectionEvent {
     Up,
     Down,
 }
@@ -37,19 +37,31 @@ type DbusPath = Path<'static>;
 type DbusPathMessage = (dbus::message::Message, (DbusPath,));
 type DbusPathChannel = channel::mpsc::UnboundedReceiver<DbusPathMessage>;
 
+/// Dbus event stream
+struct DbusEventStream {
+    /// Stop watching events token
+    signal: MsgMatch,
+    /// Actual stream
+    stream: DbusPathChannel,
+}
+
+impl DbusEventStream {
+    fn from_dbus_match((signal, stream): (MsgMatch, DbusPathChannel)) -> Self {
+        Self { signal, stream }
+    }
+}
+
+/// Watch for dbus events and execute user's scripts
 struct Watcher {
-    // Dbus Connection
+    /// Dbus Connection
     conn: std::sync::Arc<dbus::nonblock::SyncConnection>,
-    // Map connection uuid to their config
-    name_config_map: HashMap<String, ConnectionConfig>,
-    // Map nm devices to their uuid
-    active_devices_map: HashMap<DbusPath, String>,
-    // Signal token
-    conn_added_sig: MsgMatch,
-    conn_removed_sig: MsgMatch,
-    // Event streams
-    conn_added_stream: DbusPathChannel,
-    conn_removed_stream: DbusPathChannel,
+    /// Map connection uuid to their config
+    user_config_map: HashMap<String, ConnectionConfig>,
+    /// Map nm devices to their uuid
+    up_map: HashMap<DbusPath, String>,
+    /// Event streams
+    iface_add_ev: DbusEventStream,
+    iface_del_ev: DbusEventStream,
 }
 
 impl Watcher {
@@ -64,147 +76,131 @@ impl Watcher {
             panic!("Lost connection to D-Bus: {}", err);
         });
 
-        let (conn_added_sig, conn_added_stream): (MsgMatch, DbusPathChannel) = conn
-            .clone()
-            .add_match(MatchRule::new_signal(
-                "org.freedesktop.DBus.ObjectManager",
-                "InterfacesAdded",
-            ))
-            .await?
-            .stream();
-        let (conn_removed_sig, conn_removed_stream): (MsgMatch, DbusPathChannel) = conn
-            .clone()
-            .add_match(MatchRule::new_signal(
-                "org.freedesktop.DBus.ObjectManager",
-                "InterfacesRemoved",
-            ))
-            .await?
-            .stream();
+        // Create dbus InterfacesAdded event stream
+        let iface_add_ev = DbusEventStream::from_dbus_match(
+            conn.clone()
+                .add_match(MatchRule::new_signal(
+                    "org.freedesktop.DBus.ObjectManager",
+                    "InterfacesAdded",
+                ))
+                .await?
+                .stream(),
+        );
+
+        // Create dbus InterfacesRemoved event stream
+        let iface_del_ev = DbusEventStream::from_dbus_match(
+            conn.clone()
+                .add_match(MatchRule::new_signal(
+                    "org.freedesktop.DBus.ObjectManager",
+                    "InterfacesRemoved",
+                ))
+                .await?
+                .stream(),
+        );
+
+        let user_config_map = config.connections;
+        let up_map = HashMap::new();
 
         Ok(Self {
-            conn: conn.clone(),
-            name_config_map: config.connections,
-            active_devices_map: HashMap::new(),
-            conn_added_sig,
-            conn_removed_sig,
-            conn_added_stream,
-            conn_removed_stream,
+            conn,
+            user_config_map,
+            up_map,
+            iface_add_ev,
+            iface_del_ev,
         })
     }
 
     async fn run(&mut self) -> Result<()> {
-        // TODO Call for all already active connections
-        //         let proxy = Proxy::new(
-        //             DBUS_NM_OBJECT_NAME,
-        //             DBUS_NM_PATH,
-        //             Duration::from_millis(DEFAULT_TIMEOUT),
-        //             self.conn.clone(),
-        //         );
-        //         let (present_devices,): (Vec<DbusPath>,) = proxy
-        //             .method_call(DBUS_NM_OBJECT_NAME, "GetAllDevices", ())
-        //             .await?;
-        //         for device in present_devices {
-        //             // Consider current present device up
-        //             self.active_connection_mutation(ActiveConnectionAction::Up, device).await;
-        //         }
-
-        // Run forever.
-        // TODO setup a unix signal handler and use a channel to close soft softly
+        // TODO Call all already active connections
+        // TODO setup a unix signal handler and use a channel to quit service softly
         loop {
             let (act, msg) = tokio::select! {
-                Some(msg) =  self.conn_added_stream.next() => {
-                    (ActiveConnectionAction::Up, msg)
+                Some(msg) = self.iface_add_ev.stream.next() => {
+                    (ConnectionEvent::Up, msg)
                 },
-                Some(msg) = self.conn_removed_stream.next() => {
-                    (ActiveConnectionAction::Down, msg)
+                Some(msg) = self.iface_del_ev.stream.next() => {
+                    (ConnectionEvent::Down, msg)
                 },
                 else => break,
             };
-            let active_connection = (msg.1).0;
+            let connection = (msg.1).0;
             // Consider only active connections
-            if active_connection
-                .as_cstr()
-                .to_str()
-                .unwrap()
-                .to_owned()
-                .starts_with("/org/freedesktop/NetworkManager/ActiveConnection/")
-            {
-                self.active_connection_mutation(act, active_connection)
-                    .await;
+            if connection.starts_with(DBUS_NM_ACTIVE_CONNECTION_PATH) {
+                // if the event correspond to something in up_map
+                // we call associated command
+                if let Some(conn_uuid) = self.connection_event(act.clone(), connection).await {
+                    self.run_conn_cmd(&conn_uuid, &act).await;
+                }
             }
         }
 
+        // Clean before exit
         self.teardown().await?;
         Ok(())
     }
 
     async fn teardown(&mut self) -> Result<()> {
-        self.conn.remove_match(self.conn_added_sig.token()).await?;
+        info!("Tearing down dbus event streams");
         self.conn
-            .remove_match(self.conn_removed_sig.token())
+            .remove_match(self.iface_add_ev.signal.token())
+            .await?;
+        self.conn
+            .remove_match(self.iface_del_ev.signal.token())
             .await?;
         Ok(())
     }
 
-    async fn get_connection_uuid(&self, active_connection: DbusPath) -> Option<String> {
+    async fn connection_uuid(&self, act_conn: &DbusPath) -> Option<String> {
+        let dbus_endpoint = "org.freedesktop.NetworkManager.Connection.Active";
         let conn_proxy = Proxy::new(
             DBUS_NM_OBJECT_NAME,
-            active_connection,
-            Duration::from_millis(DEFAULT_TIMEOUT),
+            act_conn,
+            Duration::from_millis(DBUS_DEFAULT_TIMEOUT),
             self.conn.clone(),
         );
-        match conn_proxy
-            .get::<String>("org.freedesktop.NetworkManager.Connection.Active", "Uuid")
+        conn_proxy
+            .get::<String>(dbus_endpoint, "Uuid")
             .await
-        {
-            Ok(r) => Some(r),
-            Err(_e) => None,
+            .ok_or_log_err("connection_uuid:")
+    }
+
+    async fn connection_event(
+        &mut self,
+        action: ConnectionEvent,
+        nm_conn: DbusPath,
+    ) -> Option<String> {
+        match action {
+            // Iface is up
+            ConnectionEvent::Up => self.connection_uuid(&nm_conn).await.map(|uuid| {
+                self.up_map.entry(nm_conn).or_insert_with(|| uuid.clone());
+                uuid
+            }),
+            // Iface is down
+            ConnectionEvent::Down => self.up_map.remove_entry(&nm_conn).map(|e| e.1),
         }
     }
 
-    async fn active_connection_mutation(
-        &mut self,
-        action: ActiveConnectionAction,
-        active_connection: DbusPath,
-    ) {
-        match action {
-            ActiveConnectionAction::Up => {
-                if let Some(uuid) = self.get_connection_uuid(active_connection.clone()).await {
-                    self.active_devices_map
-                        .entry(active_connection)
-                        .or_insert_with(|| uuid.clone());
-                    self.call_user_defined_script(&uuid, ActiveConnectionAction::Up)
-                        .await;
-                }
-            }
-            ActiveConnectionAction::Down => {
-                if let Some(uuid) = self
-                    .active_devices_map
-                    .remove_entry(&active_connection)
-                    .map(|e| e.1)
-                {
-                    self.call_user_defined_script(&uuid, ActiveConnectionAction::Down)
-                        .await;
-                }
-            }
-        };
-    }
-
-    async fn call_user_defined_script(&self, uuid: &str, action: ActiveConnectionAction) {
-        if let Some(conn_params) = self.name_config_map.get(uuid) {
-            let script = match action {
-                ActiveConnectionAction::Up => &conn_params.up_script,
-                ActiveConnectionAction::Down => &conn_params.down_script,
+    async fn run_conn_cmd(&self, uuid: &str, action: &ConnectionEvent) {
+        if let Some(conn_params) = self.user_config_map.get(uuid) {
+            let cmd = match action {
+                ConnectionEvent::Up => &conn_params.up_script,
+                ConnectionEvent::Down => &conn_params.down_script,
             };
             info!("{} {:?}", conn_params.name, action);
-            Command::new("/bin/sh").arg("-c").arg(script).env("CONNECTION_NAME", &conn_params.name).env("CONNECTION_CONTEXT", &conn_params.context).spawn().expect("Command failed.");
+            Command::new("/bin/sh")
+                .arg("-c")
+                .arg(cmd)
+                .env("CONNECTION_NAME", &conn_params.name)
+                .env("CONNECTION_CONTEXT", &conn_params.context)
+                .spawn()
+                .expect("Command failed.");
         }
     }
 }
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
-    // Load the CLI parameters from the yaml file
+    // Load CLI parameters from yaml file
     let cli = Clapp::new(crate_name!()).version(crate_version!()).arg(
         Arg::with_name("config")
             .short("c")
@@ -216,7 +212,7 @@ pub async fn main() -> Result<()> {
     let _app_name = cli.get_name().to_owned();
     let matches = cli.get_matches();
 
-    // Retrieve the config file path
+    // Retrieve config file path
     let config_filename = match matches.value_of("config") {
         Some(c) => Ok(c.to_owned()),
         None => var(CONFIG_FILE_ENV).map_err(|_e| {
@@ -227,16 +223,36 @@ pub async fn main() -> Result<()> {
         }),
     }?;
 
-    // Parse the configuration
+    // Parse configuration
     let config = Config::from_file(&config_filename)?;
 
-    // Set the logging verbosity
+    // Set logging verbosity
     set_var("RUST_LOG", "info");
-    // Initialize the logger
+    // Initialize logger
     env_logger::init();
 
     let mut watcher = Watcher::from_config(config).await?;
-    info!("Watching");
+    info!("Watching for NetworkManager events");
     watcher.run().await?;
     Ok(())
+}
+
+/// Log errors on Result
+pub trait ResultOkLogErrExt<T> {
+    fn ok_or_log_err(self, msg: &str) -> Option<T>;
+}
+
+impl<T, E> ResultOkLogErrExt<T> for Result<T, E>
+where
+    E: ::std::fmt::Display,
+{
+    fn ok_or_log_err(self, msg: &str) -> Option<T> {
+        match self {
+            Ok(v) => Some(v),
+            Err(e) => {
+                error!("{}: {}", msg, e);
+                None
+            }
+        }
+    }
 }
